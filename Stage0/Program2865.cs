4633063f13f33b25f341f6b3e5e0b0a9ba8a32b0
simcharozenvaser <sr0527652555@gmail.com>
2022-10-24 11:29:12 +0300

    inital code +Welcome functions

---
+++ Stage0/Program2865.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+  partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2865();
+            Welcome9142();
+            Console.ReadKey();
+        }
+        static partial void Welcome9142();
+        private static void Welcome2865()
+        {
+            Console.Write("Enter your name: ");
+            string n = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", n);
+        }
+    }
+}
