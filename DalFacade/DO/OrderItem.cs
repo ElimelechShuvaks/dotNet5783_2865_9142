79fc2809b576adce3b85b324b4b22e4cf4920501
Elimelech <34412as@gmail.com>
2022-11-02 11:33:01 +0200

    definition of OrderItem class.

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -2,8 +2,19 @@
 namespace DO;
 
 /// <summary>
-/// 
+/// data of Order item.
 /// </summary>
 public struct OrderItem
 {
+    private int id;
+    private int productId;
+    private int orderId;
+    private double price;
+    private int amount;
+
+    public int Id { get => id; set => id = value; }
+    public int ProductId { get => productId; set => productId = value; }
+    public int OrderId { get => orderId; set => orderId = value; }
+    public double Price { get => price; set => price = value; }
+    public int Amount { get => amount; set => amount = value; }
 }
