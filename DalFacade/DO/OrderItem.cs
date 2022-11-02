93c6d1552862be27c4a3cdab1701e4d3816ccd50
simcharozenvaser <sr0527652555@gmail.com>
2022-11-02 18:18:26 +0200

    add part of class config

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,5 +1,4 @@
-﻿
-namespace DO;
+﻿namespace DO;
 
 /// <summary>
 /// data of Order item.
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,5 +1,32 @@
-﻿namespace Dal;
+﻿using DO;
+
+namespace Dal;
 
 internal static class DataSource
 {
+    //Because its Random its class we need to make a =new();.
+    public static readonly Random _random_ = new Random(); 
+
+    //the arrys.
+    internal static Product[] Products = new Product[50];   
+
+    internal static Order[] Orders = new Order[100];    
+
+    internal static OrderItem[] OrderItems = new OrderItem[200];  
+ 
+    //class Config.
+    internal static class Config
+    {
+        //Counter for amount.
+        internal static int counterProduct=0;   
+
+        internal static int counterOrders=0;    
+
+        internal static int counterOrderitem=0; 
+
+
+
+    }
+
+
 }
