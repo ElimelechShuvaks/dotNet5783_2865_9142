cb3dc7a4f1a294641dddfd00cc37de85a2fb2a36
Elimelech <34412as@gmail.com>
2022-11-03 13:35:41 +0200

    start to build the main.

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,4 +1,11 @@
-﻿using DO;
+﻿
 
-OrderItem o = new();
-    Console.WriteLine(o);
+using Dal;
+
+Console.WriteLine(@" for which struct you want a test?
+ press 1 to check a Product.
+ press 2 to check an Order.
+ press 3 to check an Order Item.
+"
+);
+
