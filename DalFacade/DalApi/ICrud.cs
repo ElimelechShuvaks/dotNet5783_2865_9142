3df74b5cb6bf35c970544564b1bc1bfb95fd2aea
Elimelech <34412as@gmail.com>
2022-11-13 20:33:07 +0200

    start build the interface

---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi;
+
+public interface ICrud<T>
+{
+    public int add(T entity);
+    public T get(int id);
+    public IEnumerable<T> getArray();
+    public void delete(int id);
+    public void update(T entity);
+}
---
+++ DalFacade/DalApi/IOrderItem.cs
@@ -0,0 +1,9 @@
+﻿
+using DO;
+namespace DalApi;
+
+internal interface IOrderItem : ICrud<OrderItem>
+{
+    public OrderItem getBuy_2Id(int pId, int oId);
+    public List<OrderItem> getItemArray(int orderId);
+}
