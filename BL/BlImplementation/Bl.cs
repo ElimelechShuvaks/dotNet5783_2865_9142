994a86e8cac21be5fb5bf7b61bf97759e2e9a343
Elimelech <34412as@gmail.com>
2022-11-23 02:01:58 +0200

    build the IBl interface and implementation.

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,15 @@
+﻿
+using BlApi;
+namespace BlImplementation;
+
+/// <summary>
+/// inplementation of IBl interface, that holds the 3 main entities Product, Order and Cart.
+/// </summary>
+sealed public class Bl : IBl
+{
+    public IProduct Product => new Product();
+
+    public IOrder Order => new Order();
+
+    public ICart Cart => new Cart();
+}
