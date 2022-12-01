499a9ef9f22665bbaee915b62358fa932c017a7c
Elimelech <34412as@gmail.com>
2022-12-01 17:47:57 +0200

    add comments in the DO entety

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -5,12 +5,39 @@
 /// </summary>
 public struct Order
 {
+    /// <summary>
+    /// order id
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Customer name
+    /// </summary>
     public string? CustomerName { get; set; }
+
+    /// <summary>
+    /// Customer email
+    /// </summary>
     public string? CustomerEmail { get; set; }
+
+    /// <summary>
+    /// Customer adress
+    /// </summary>
     public string? CustomerAdress { get; set; }
+
+    /// <summary>
+    /// Order date
+    /// </summary>
     public DateTime? OrderDate { get; set; }
+
+    /// <summary>
+    /// Ship date
+    /// </summary>
     public DateTime? ShipDate { get; set; }
+
+    /// <summary>
+    /// Delivery date
+    /// </summary>
     public DateTime? DeliveryDate { get; set; }
 
     public override string ToString()
--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -5,11 +5,34 @@
 /// </summary>
 public struct Product
 {
+    /// <summary>
+    /// Product id
+    /// </summary>
     public int ProductId { get; set; }
+
+    /// <summary>
+    /// product name
+    /// </summary>
     public string? Name { get; set; }
+
+    /// <summary>
+    /// product price
+    /// </summary>
     public double Price { get; set; }
+
+    /// <summary>
+    /// product category
+    /// </summary>
     public Categories? Category { get; set; }
+
+    /// <summary>
+    /// product image
+    /// </summary>
     public string? Image { get; set; }
+
+    /// <summary>
+    /// amount in stock
+    /// </summary>
     public int InStock { get; set; }
 
     public override string ToString() => $@"Product ProductId: {ProductId}
