c55ac8b2a5cb8da84c29f6495e9d168926f3247d
Elimelech <34412as@gmail.com>
2022-12-08 01:59:02 +0200

    prevent non numbers input

--- PL/Products/ProductWindow.xaml.cs
+++ PL/Products/ProductWindow.xaml.cs
@@ -5,87 +5,109 @@ using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics.Metrics;
 using System.Numerics;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Input;
 
+namespace PL.Products;
 
-namespace PL.Products
+/// <summary>
+/// Interaction logic for Product.xaml
+/// </summary>
+public partial class ProductWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for Product.xaml
-    /// </summary>
-    public partial class ProductWindow : Window
+    IBl localBl;
+
+    BO.Product product1 = new BO.Product();
+    public ProductWindow(IBl bl)
     {
-        IBl localBl;
+        localBl = bl;
 
-        BO.Product product1 = new BO.Product();
-        public ProductWindow(IBl bl)
+        InitializeComponent();
+
+        categoryComboBox.ItemsSource = Enum.GetValues(typeof(BO.Categories));
+
+        productWindowButton.Content = "Add";
+    }
+    public ProductWindow(IBl bl, int ProductId)
+    {
+        localBl = bl;
+        InitializeComponent();
+
+        categoryComboBox.ItemsSource = Enum.GetValues(typeof(BO.Categories));
+        productWindowButton.Content = "Update";
+        product1 = localBl.Product.ProductDetailsManger(ProductId);
+
+        idTextBox.Text = product1.Id.ToString();
+        categoryComboBox.Text = product1.Category.ToString();
+        nameTextBox.Text = product1.Name!.ToString();
+        priceTextBox.Text = product1.Price.ToString();
+        inStockTextBox.Text = product1.InStock.ToString();
+
+
+    }
+
+    private void productWindowButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (productWindowButton.Content.ToString() == "Add")
         {
-            localBl = bl;
-
-            InitializeComponent();
-
-            categoryComboBox.ItemsSource = Enum.GetValues(typeof(BO.Categories));
-
-            productWindowButton.Content = "Add";
-        }
-        public ProductWindow(IBl bl, int ProductId)
-        {
-            localBl = bl;
-            InitializeComponent();
-
-            categoryComboBox.ItemsSource = Enum.GetValues(typeof(BO.Categories));
-            productWindowButton.Content = "Update";
-            product1 = localBl.Product.ProductDetailsManger(ProductId);
-
-            idTextBox.Text = product1.Id.ToString();
-            categoryComboBox.Text = product1.Category.ToString();
-            nameTextBox.Text = product1.Name!.ToString();
-            priceTextBox.Text = product1.Price.ToString();
-            inStockTextBox.Text = product1.InStock.ToString();
-
-
-        }
-
-        private void productWindowButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (productWindowButton.Content.ToString() == "Add")
+            BO.Product product = new BO.Product()
             {
-                BO.Product product = new BO.Product()
-                {
-                    Category = OtherFunctions.CategoryParse(categoryComboBox),
-                    Name = nameTextBox.Text.ToString(),
-                    Id = int.Parse(idTextBox.Text),
-                    Price = int.Parse(priceTextBox.Text),
-                    InStock = int.Parse(inStockTextBox.Text)
-                };
+                Category = OtherFunctions.CategoryParse(categoryComboBox),
+                Name = nameTextBox.Text.ToString(),
+                Id = int.Parse(idTextBox.Text),
+                Price = double.Parse(priceTextBox.Text),
+                InStock = int.Parse(inStockTextBox.Text)
+            };
 
-                try
-                {
-                    localBl.Product.AddProduct(product);
-                    Close();
-                }
-                catch (BlExceptions ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+            try
+            {
+                localBl.Product.AddProduct(product);
+                Close();
             }
-            else
+            catch (BlExceptions ex)
             {
-                try
-                {
-                    product1.Category = OtherFunctions.CategoryParse(categoryComboBox);
-                    product1.Name = nameTextBox.Text.ToString();
-                    product1.Price = int.Parse(priceTextBox.Text);
-                    product1.InStock = int.Parse(inStockTextBox.Text);
-
-                    localBl.Product.UpdateProduct(product1);
-                    Close();
-                }
-                catch (BlExceptions ex)
-                {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+        else
+        {
+            try
+            {
+                product1.Category = OtherFunctions.CategoryParse(categoryComboBox);
+                product1.Name = nameTextBox.Text.ToString();
+                product1.Price = double.Parse(priceTextBox.Text);
+                product1.InStock = int.Parse(inStockTextBox.Text);
+
+                localBl.Product.UpdateProduct(product1);
+                Close();
+            }
+            catch (BlExceptions ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    /// <summary>
+    /// private helpe function to prevent the user to type non number digits in text box.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void IntegerValidationTextBox(object sender, TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
+
+    /// <summary>
+    /// private helpe function to prevent the user to type non numbers or decimal numbers in text box.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DoubleValidationTextBox(object sender, TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^0-9.]+");
+        e.Handled = regex.IsMatch(e.Text);
     }
 }
